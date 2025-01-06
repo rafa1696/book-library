de53d8644e76104c3e8bb1f2bbc651cb56a96906
rafa1696 <rafaferrdev@gmail.com>
2025-01-06 08:32:51 -0300

    feat: refatorar DiaryEntry para separar título e estados de texto

--- src/components/DiaryEntry/DiaryEntry.tsx
+++ src/components/DiaryEntry/DiaryEntry.tsx
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useBookLibraryContext } from "../../context/BookLibraryContext";
 import { ReadingDiaryEntry } from "../../types/ReadingDiaryEntry.type";
-import styles from "./DiaryEntry.module.css";
 import { returnRandomNumber } from "../../utils/returnRandomNumber";
+import styles from "./DiaryEntry.module.css";
 
 const DiaryEntry = () => {
   const { diaryEntryId } = useParams<{ diaryEntryId: string }>();
   const { readingDiary, saveReadingDiary } = useBookLibraryContext();
   const [entry, setEntry] = useState<ReadingDiaryEntry | null>(null);
-  const [text, setText] = useState("");
+
+  const [entryTitle, setEntryTitle] = useState("");
+  const [entryText, setEntryText] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     const foundEntry =
       readingDiary && readingDiary.find((entry) => entry.id === diaryEntryId);
 
-    console.log("foundEntry", foundEntry);
-
     if (foundEntry) {
       setEntry(foundEntry);
-      setText(foundEntry.entry);
+      setEntryText(foundEntry.entry);
     } else {
       if (diaryEntryId) {
         setEntry({
@@ -36,29 +36,31 @@ const DiaryEntry = () => {
 
   const handleSave = () => {
     // TODO - Trocar nome de entry para diaryEntryText ou algo parecido
-    if (entry && text.length > 0) {
-      saveReadingDiary({ ...entry, entry: text });
+    if (entry && entryText.length > 0) {
+      saveReadingDiary({ ...entry, title: entryTitle, entry: entryText });
       navigate("/reading-diary");
     }
   };
 
-  useEffect(() => {
-    console.log("entry", entry);
-  }, [entry]);
-
-  // if (!entry) {
-  //   return <div>Entrada não encontrada</div>;
-  // }
-
   return (
     <section className={styles.container}>
-      <span className={styles.container_entryTitle}>
+      <span className={styles.container_sectionTitle}>
         <h1>Entrada do Diário</h1>
       </span>
-      <textarea
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <span className={styles.container_entryTitle}>
+        <h2>Título da entrada</h2>
+        <textarea
+          value={entryTitle}
+          onChange={(e) => setEntryTitle(e.target.value)}
+        />
+      </span>
+      <span className={styles.container_entryText}>
+        <h2>Texto</h2>
+        <textarea
+          value={entryText}
+          onChange={(e) => setEntryText(e.target.value)}
+        />
+      </span>
       <button onClick={handleSave}>Salvar</button>
     </section>
   );
