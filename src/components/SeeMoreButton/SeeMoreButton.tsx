a5f21befefa5e5d037b78da33bc53a139d8869e6
Rafael Ferreira <rafaferrdev@gmail.com>
2025-02-05 19:56:24 -0300

    feat: estilizar botão "Ver mais" e ajustar layout de cartões e galeria

--- src/components/SeeMoreButton/SeeMoreButton.tsx
+++ src/components/SeeMoreButton/SeeMoreButton.tsx
@@ -1,10 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import { NavigationRoutes } from "../../enums/NavigationRoutes.enum";
 
+import styles from "./SeeMoreButton.module.css";
+
 const SeeMoreButton = () => {
   const navigate = useNavigate();
 
-  return <button onClick={() => navigate(NavigationRoutes.MyBooks)}>+</button>;
+  return (
+    <button
+      className={styles.container}
+      onClick={() => navigate(NavigationRoutes.MyBooks)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        {/* Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+        License - https://fontawesome.com/license/free Copyright 2025 Fonticons,
+        Inc. */}
+        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
+      </svg>
+    </button>
+  );
 };
 
 export default SeeMoreButton;
