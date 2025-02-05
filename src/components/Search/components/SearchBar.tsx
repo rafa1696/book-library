7f47f57fa34ce6f90b14480f77e5cfa5bfa9f89d
Rafael Ferreira <rafaferrdev@gmail.com>
2025-02-05 20:12:28 -0300

    feat: refatorar componente de busca e adicionar SearchBar

---
+++ src/components/Search/components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import styles from "./SearchBar.module.css";
+
+interface ISearchBar {
+  handleSearch: () => void;
+  input: string;
+  setInput: (value: React.SetStateAction<string>) => void;
+}
+
+const SearchBar: FC<ISearchBar> = ({ setInput, handleSearch, input }) => {
+  return (
+    <div className={styles.container}>
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+        placeholder="Busque um livro..."
+        type="text"
+        value={input}
+      />
+      <button onClick={handleSearch}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          {/* Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+          License - https://fontawesome.com/license/free Copyright 2025
+          Fonticons, Inc. */}
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
