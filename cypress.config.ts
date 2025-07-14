994a2b1cae11f99f8bdae5efb507b265300bf532
Rafael Ferreira <rafaferrdev@gmail.com>
2025-07-14 15:55:11 -0300

    feat: add Cypress artifacts to .gitignore and configure reporter options in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,15 @@ export default defineConfig({
 			framework: 'react',
 			bundler: 'vite',
 		},
+		video: true,
+		reporter: 'mochawesome',
+		reporterOptions: {
+			reportDir: 'cypress/results',
+			overwrite: false,
+			html: true,
+			json: false,
+			timestamp: 'mmddyyyy_HHMMss',
+		},
 	},
 
 	e2e: {
